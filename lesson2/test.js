5a3d8d4346941e65e532e87348bce584158f403f
Mark Biesheuvel <mark.biesheuvel@hotmail.com>
2012-04-15 12:41:55 +0200

    Added tester
    Signed-off-by: Mark Biesheuvel <mark.biesheuvel@hotmail.com>

---
+++ lesson2/test.js
@@ -0,0 +1,15 @@
+compile = require('./MUS compiler.js');
+
+var melody_mus = 
+    { tag: 'seq',
+      left: 
+       { tag: 'seq',
+         left: { tag: 'note', pitch: 'a4', dur: 250 },
+         right: { tag: 'note', pitch: 'b4', dur: 250 } },
+      right:
+       { tag: 'seq',
+         left: { tag: 'note', pitch: 'c4', dur: 500 },
+         right: { tag: 'note', pitch: 'd4', dur: 500 } } };
+
+console.log(melody_mus);
+console.log(compile(melody_mus));
