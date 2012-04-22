aa45deaa0bea3b37bf3439794ea4f81f7829b99c
Mark Biesheuvel <mark.biesheuvel@hotmail.com>
2012-04-22 09:51:34 +0200

    Start of level 3
    Signed-off-by: Mark Biesheuvel <mark.biesheuvel@hotmail.com>

---
+++ lesson3/test.js
@@ -0,0 +1,18 @@
+var PEG = require('./peg.js');
+var assert = require('assert');
+var fs = require('fs'); // for loading files
+
+fs.readFile('scheem.peg', 'ascii', function(err, data) {
+    // Show the PEG grammar file
+    console.log(data);
+    // Create my parser
+    var parse = PEG.buildParser(data).parse;
+    // Do a test
+    assert.deepEqual( parse("a"), "a", "Parse atom" );
+    assert.deepEqual( parse("  a "), "a", "Parse atom with whitespace" );
+    assert.deepEqual( parse(" ( a) "), ["a"], "Parse single atom list with whitespace" );
+    assert.deepEqual( parse("(a (b c d))"), ["a", ["b", "c", "d"]], "Parse simple expression" );
+    assert.deepEqual( parse("  (  a  b  c ) "), ["a", "b", "c"], "Parse expression with a lot of whitepsaces");
+    assert.deepEqual( parse("(a (b c) (c d e))"), ["a", ["b", "c"], ["c", "d", "e"]], "Parse complex expression" );
+    assert.deepEqual( parse("(a\t(b c)\r\n(c d e))"), ["a", ["b", "c"], ["c", "d", "e"]], "Parse complex expression" );
+});
