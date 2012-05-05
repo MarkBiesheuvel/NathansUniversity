e66284536272b6fbdcb41102c92a2f4eb1e92f84
Mark Biesheuvel <mark.biesheuvel@hotmail.com>
2012-05-05 10:05:36 +0200

    Got stuck a week ago. Forgot to commit.
    Signed-off-by: Mark Biesheuvel <mark.biesheuvel@hotmail.com>

--- lesson3/test.js
+++ lesson3/test.js
@@ -14,5 +14,7 @@ fs.readFile('scheem.peg', 'ascii', function(err, data) {
     assert.deepEqual( parse("(a (b c d))"), ["a", ["b", "c", "d"]], "Parse simple expression" );
     assert.deepEqual( parse("  (  a  b  c ) "), ["a", "b", "c"], "Parse expression with a lot of whitepsaces");
     assert.deepEqual( parse("(a (b c) (c d e))"), ["a", ["b", "c"], ["c", "d", "e"]], "Parse complex expression" );
-    assert.deepEqual( parse("(a\t(b c)\r\n(c d e))"), ["a", ["b", "c"], ["c", "d", "e"]], "Parse complex expression" );
+    assert.deepEqual( parse("(a\t(b c)\r\n(c d e))"), ["a", ["b", "c"], ["c", "d", "e"]], "Parse complex expression with all kind of whitespace" );
+    assert.deepEqual( parse("(a\t(b c)\r\n(c d e)) ;; comments start here"), ["a", ["b", "c"], ["c", "d", "e"]], "Parse complex expression with comment" );
+    assert.deepEqual( parse("(a;; comment \r\n(b c d))"), ["a", ["b", "c", "d"]], "Parse comment terminated by newline" );
 });
